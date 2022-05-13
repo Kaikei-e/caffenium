0fdc5279c0d6cff5b633b918d6bf292b81696e80
Kaikei-e <e_kaikei@protonmail.com>
2022-05-13 23:11:58 +0900

    Update: implementing time parsing API

--- src/apiServer/router.nim
+++ src/apiServer/router.nim
@@ -1,4 +1,9 @@
 import htmlgen, jester, json
+import std/times
+
+type CaffeineData* = object
+  startDate: DateTime
+  endDate: DateTime
 
 proc router*() = 
   settings:
@@ -16,5 +21,25 @@ proc router*() =
         let headers = [("Content-type", "application/json; charset=utf-8")]
         let response = %*{"message": "An error occured"}
         resp Http400, headers, $response
+    
+    post "/api/v1/calculate":
+      try:
+        let params = request.body.parseJson
+        let sDate = params["start_date"].getStr
+        let eDate = params["end_date"].getStr
 
+        let parsedSDate = parse(sDate, "yyyy-MM-dd HH:mm:ss")
+        let parsedEDate = parse(eDate, "yyyy-MM-dd HH:mm:ss")
+
+        let caffeData = CaffeineData(startDate: parsedSDate, endDate: parsedEDate)
+
+        echo caffeData
+        let response = %*{"time_formatted": getDateStr(caffeData.startDate)}
+        resp response
+
+      except Exception:
+        let headers = [("Content-type", "application/json; charset=utf-8")]
+        let response = %*{"message": "An error occured"}
+        resp Http400, headers, $response
+        
   
