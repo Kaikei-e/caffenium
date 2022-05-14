afa18fea81a1ec29fa19a59b660893839386810a
Kaikei-e <e_kaikei@protonmail.com>
2022-05-15 03:41:30 +0900

    Update: implementing the calculator.

--- src/calculator/calcLogic.nim
+++ src/calculator/calcLogic.nim
@@ -1,16 +1,18 @@
 import json
 import formSorter
 import ../bindTypes/apiType
+import ../bindTypes/caffeStructs
 import coreCalculator
 
-proc decayCalculator*(caffeData: JsonNode): seq[FormData] =
+proc decayCalculator*(caffeData: JsonNode): seq[TheDecay] =
+  var decayData: seq[TheDecay]
 
   let sortedData = formSorter(caffeData)
 
   for item in sortedData:
-    coreCalculator(item)
+    let decayData = coreCalculator(item)
 
     echo item
 
 
-  sortedData
+  decayData
