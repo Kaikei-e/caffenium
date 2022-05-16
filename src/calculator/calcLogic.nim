657d9f2b757f9ce3be9e2608bd82dd42b828e233
Kaikei-e <e_kaikei@protonmail.com>
2022-05-16 15:05:26 +0000

    Fix: overwrite array issue

--- src/calculator/calcLogic.nim
+++ src/calculator/calcLogic.nim
@@ -1,19 +1,25 @@
 import json
+import std/tables, std/sequtils
+import times
 import formSorter
-import ../bindTypes/apiType
 import ../bindTypes/caffeStructs
 import coreCalculator
 import sumDecayList
 
 proc decayCalculator*(caffeData: JsonNode): seq[TheDecay] =
   var decayData: seq[TheDecay]
+  var summedResult = initTable[DateTime, float64]()
 
   let sortedData = formSorter(caffeData)
 
+  var tmpSeq = newSeq[TheDecay]()
   for item in sortedData:
-    decayData = coreCalculator(item)
+    tmpSeq = coreCalculator(item)
+    decayData.add(tmpSeq)
 
     # echo item
+
+  
 
   let dd = makeDecaySumList(decayData)
   #echo dd
